2391e5e118dc9a03c895b9dafc2446e9641362a9
Sangeethks <sangeethks.in@gmail.com>
2018-05-01 13:19:14 +0530

    Dev build working fine

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -8,7 +8,7 @@ function resolve(dir) {
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: './src'
   },
   output: {
     path: config.prod.assetsRoot,
--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 var config = {
   dev: {
     host: 'localhost',
-    port: 3004,
+    port: 3005,
     devtool: 'cheap-module-eval-source-map'
   },
   prod: {
